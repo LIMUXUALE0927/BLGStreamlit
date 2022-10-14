27129bbb67c2797f11f95ba378c484ee50153ed4
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2022-10-15 01:51:03 +0800

    Update 5_SearchEngine.py

--- pages/5_SearchEngine.py
+++ pages/5_SearchEngine.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown("# 英雄联盟数据搜索引擎 🎉")
 
 st.markdown("### 2支队伍的历史对阵信息")
-st.info("例: 想查询RNG和EDG的历史交手比赛信息，在下方2个输入框输入`RNG`和`EDG`，再点击生成查询链接即可，输入顺序不影响结果。")
+st.info("例: 想查询RNG和EDG的历史交手比赛信息，在下方2个输入框输入 RNG 和 EDG ，再点击生成查询链接即可，输入顺序不影响结果。")
 team1 = st.text_input("请输入队伍1名称")
 team2 = st.text_input("请输入队伍2名称")
 
