14a0e046535f8f1a57155efe175a7873b78e99d3
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2022-10-15 01:52:14 +0800

    Update 5_SearchEngine.py

--- pages/5_SearchEngine.py
+++ pages/5_SearchEngine.py
@@ -4,8 +4,12 @@ st.markdown("# 英雄联盟数据搜索引擎 🎉")
 
 st.markdown("### 2支队伍的历史对阵信息")
 st.info("例: 想查询RNG和EDG的历史交手比赛信息，在下方2个输入框输入 RNG 和 EDG ，再点击生成查询链接即可，输入顺序不影响结果。")
-team1 = st.text_input("请输入队伍1名称")
-team2 = st.text_input("请输入队伍2名称")
+
+col1, col2 = st.columns(2)
+with col1:
+    team1 = st.text_input("请输入队伍1名称")
+with col2:
+    team2 = st.text_input("请输入队伍2名称")
 
 s = f"https://lol.fandom.com/Special:RunQuery/MatchHistoryGame?MHG%5Bspl%5D=yes&MHG%5Bteam1%5D={team1}&MHG%5Bteam2%5D={team2}&MHG%5Bpreload%5D=TeamHeadToHead&_run="
 
