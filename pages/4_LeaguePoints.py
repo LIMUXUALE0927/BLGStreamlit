a7126df7babc1cebc21c131f7a45ecb19beb9f45
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2022-10-29 04:03:41 +0800

    Update 4_LeaguePoints.py

--- pages/4_LeaguePoints.py
+++ pages/4_LeaguePoints.py
@@ -8,14 +8,14 @@ apiKey = st.secrets["my_api"]
 lol_watcher = LolWatcher(apiKey)
 region = 'kr'
 
-txt = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。", "Hide on bush, T1 Gumayusi, GEN G Ruler")
+txt2 = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。", "Hide on bush, T1 Gumayusi, GEN G Ruler")
 
-namelist = txt.split(',')
-namelist = [i.strip() for i in namelist]
-namelist = [i.replace('\xa0', ' ') for i in namelist]
+namelist2 = txt2.split(',')
+namelist2 = [i.strip() for i in namelist2]
+namelist2 = [i.replace('\xa0', ' ') for i in namelist2]
 
 stats = pd.DataFrame()
-for name in namelist:
+for name in namelist2:
     try:
         summoner_id = lol_watcher.summoner.by_name(region, name)['id']
         data = lol_watcher.league.by_summoner(region, summoner_id)
