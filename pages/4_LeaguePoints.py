1f2904dd95f816fa9a4ce6ee1304658a1d440a8f
limuxuale0927 <limuxuale0927@hotmail.com>
2023-08-16 15:53:47 +0800

    Update 4_LeaguePoints.py

--- pages/4_LeaguePoints.py
+++ pages/4_LeaguePoints.py
@@ -8,7 +8,8 @@ apiKey = st.secrets["my_api"]
 lol_watcher = LolWatcher(apiKey)
 region = 'kr'
 
-txt2 = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。", "Hide on bush, T1 Gumayusi, GEN G Ruler")
+txt2 = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。",
+                    "Hide on bush, T1 Gumayusi, GEN G Ruler")
 
 namelist2 = txt2.split(',')
 namelist2 = [i.strip() for i in namelist2]
@@ -18,10 +19,12 @@ stats = pd.DataFrame()
 for name in namelist2:
     try:
         summoner_id = lol_watcher.summoner.by_name(region, name)['id']
-        data = lol_watcher.league.by_summoner(region, summoner_id)
-        stats = pd.concat([stats, data], ignore_index=True)
+        data = lol_watcher.league.by_summoner(region, summoner_id)[0]
+        record = pd.DataFrame.from_records([data])
+        stats = pd.concat([stats, record], ignore_index=True)
     except:
         continue
 
-stats = stats[stats['queueType']=='RANKED_SOLO_5x5']
-st.dataframe(stats[['summonerName', 'tier', 'leaguePoints']])
+selected_columns = ['summonerName', 'tier', 'rank', 'leaguePoints', 'wins', 'losses']
+stats = stats[selected_columns]
+st.dataframe(stats)
