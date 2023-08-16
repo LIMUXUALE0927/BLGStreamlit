678aedbf810d2bdcfa6ad8c86618dfef3e9739e6
limuxuale0927 <limuxuale0927@hotmail.com>
2023-08-16 15:24:57 +0800

    Update 3_DailyRank.py

--- pages/3_DailyRank.py
+++ pages/3_DailyRank.py
@@ -9,7 +9,8 @@ st.markdown("# 日常韩服排位数量查询 🎉")
 today = int(datetime.fromisoformat(datetime.today().strftime('%Y-%m-%d 12:00')).timestamp())
 yesterday = int(datetime.fromisoformat((datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d 12:00')).timestamp())
 
-txt = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。", "Hide on bush, T1 Gumayusi, GEN G Ruler")
+txt = st.text_area("请输入要查询的韩服ID列表，多个ID间请用英文逗号分隔。如果结果出错请刷新页面。",
+                   "Hide on bush, T1 Gumayusi")
 namelist = str(txt).split(',')
 namelist = [i.strip() for i in namelist]
 namelist = [i.replace('\xa0', ' ') for i in namelist]
@@ -19,9 +20,8 @@ my_api = st.secrets["my_api"]
 lol_watcher = LolWatcher(my_api)
 region = 'kr'
 
-st.markdown("本次查询时间段: " + (datetime.today() - timedelta(days=1)).strftime(
+st.markdown("本次查询时间段: " + (datetime.today() - timedelta(days=30)).strftime(
     '%Y-%m-%d 12:00') + " -- " + datetime.today().strftime('%Y-%m-%d 12:00'))
-
 
 rank_df = pd.DataFrame(columns=['韩服ID', '排位数量'])
 
@@ -30,9 +30,9 @@ for i in namelist:
         summoner = lol_watcher.summoner.by_name(region, i)
         puuid = summoner['puuid']
         matchlist = lol_watcher.match.matchlist_by_puuid(region='asia', puuid=puuid, type='ranked',
-                                                        start_time=yesterday, end_time=today)
+                                                         start_time=yesterday, end_time=today)
         data = pd.DataFrame({'韩服ID': [i], '排位数量': [len(matchlist)]})
-        rank_df = rank_df.append(data, ignore_index=True)
+        rank_df = pd.concat([rank_df, data], ignore_index=True)
     except:
         continue
 
