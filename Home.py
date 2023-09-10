e1e6d7a38d0564aaa1b58a6a03854aa25d7c134a
limuxuale0927 <limuxuale0927@hotmail.com>
2023-09-10 18:44:15 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,8 +14,8 @@ st.markdown(
     **👈 你可以从左侧边栏选择想要使用的程序!**
     
     ### 每个程序是干什么的?
-    - [Leaguepedia](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/Leaguepedia): 英雄联盟全球联赛数据分析程序
-    - [DailyRank](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/DailyRank): 批量查询选手每日韩服Rank数量
+    - [Leaguepedia](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/Leaguepedia): 英雄联盟全球联赛数据看板
+    - [DailyRank](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/DailyRank): 批量查询选手过去7天韩服Rank数量
     - [LeaguePoints](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/LeaguePoints): 批量查询选手韩服排位分数
     - [SearchEngine](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/SearchEngine): 特定比赛对局查询、选手主页导航
     - [PlayerStat](https://limuxuale0927-blgstreamlit-home-hdnf49.streamlitapp.com/PlayerStat): 分析联赛中各位置选手的数据并根据自定权重打分、输出雷达图
